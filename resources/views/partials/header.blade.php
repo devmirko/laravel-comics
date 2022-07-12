8bf1df2ccf203091e2c183fc886a43112d7361c9
mirkosimonetti <mirko.simonetti.ta@gmail.com>
2022-07-12 15:23:20 +0200

    Creazione parte header!!!

---
+++ resources/views/partials/header.blade.php
@@ -0,0 +1,42 @@
+<header>
+    <div class="container">
+        <!-- logo -->
+        <a href="">
+            <img src="/images/dc-logo.png" alt="questa è una immagine">
+        </a>
+        <!-- contenitore menu -->
+        <ul>
+            <li>
+                <a href="">Characters</a>
+            </li>
+            <li>
+                <a href="">Comics</a>
+            </li>
+            <li>
+                <a href="">Movies</a>
+            </li>
+            <li>
+                <a href="">TV</a>
+            </li>
+            <li>
+                <a href="">GAMES</a>
+            </li>
+            <li>
+                <a href="">COLLECTIBLES</a>
+            </li>
+            <li>
+                <a href="">VIDEOS</a>
+            </li>
+            <li>
+                <a href="">FANS</a>
+            </li>
+            <li>
+                <a href="">NEWS</a>
+            </li>
+            <li>
+                <a href="">SHOP</a>
+            </li>
+        </ul>
+
+    </div>
+  </header>
