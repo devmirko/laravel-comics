99f38578e118bb4b02e9194572c5d72e963cd871
mirkosimonetti <mirko.simonetti.ta@gmail.com>
2022-07-12 15:49:04 +0200

    inserimento main ( da inserire le card)

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -4,4 +4,29 @@
 
 @section('pageMain')
 
+<main>
+    <section id="hero">
+        <!-- immgaine hero -->
+    </section>
+    <section id="series">
+        <div id="current">CURRENT SERIES</div>
+    </section>
+    <section id="card_container">
+        <div class="container">
+            {{-- <div class="card">
+                <div class="box_image">
+                    <img :src="img" :alt="title">
+                </div>
+                <div class="box_title">
+                    <h4>{{title}}</h4>
+                </div>
+
+            </div> --}}
+        </div>
+    </section>
+    <section id="row_more">
+        <div id="read_more">READ MORE</div>
+    </section>
+</main>
+
 @endsection
