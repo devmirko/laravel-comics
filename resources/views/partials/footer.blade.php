9c0745f0e985812e3b4ba4bd0f78e8b430d9b2e0
mirkosimonetti <mirko.simonetti.ta@gmail.com>
2022-07-12 15:35:46 +0200

    inserimento footer!!!

---
+++ resources/views/partials/footer.blade.php
@@ -0,0 +1,176 @@
+<footer>
+    <section id="head_footer">
+        <div class="container_footer">
+            <ul>
+                <li>
+                    <img src="/images/buy-comics-digital-comics.png" alt="">
+                    <span>DIGITAL COMICS</span>
+                </li>
+                <li>
+                    <img src="/images/buy-comics-merchandise.png" alt="">
+                    <span>DC MERCHAINDAISE</span>
+                </li>
+                <li>
+                    <img src="/images/buy-comics-subscriptions.png" alt="">
+                    <span>SUBSCRIPTION</span>
+                </li>
+                <li>
+                    <img src="/images/buy-comics-shop-locator.png" alt="">
+                    <span>COMIC SHOP LOCATOR</span>
+                </li>
+                <li>
+                    <img src="/images/buy-dc-power-visa.svg" alt="">
+                    <span>DC POWER VISA</span>
+                </li>
+
+            </ul>
+        </div>
+    </section>
+    <section id="main_footer">
+        <div class="container">
+            <div id="nav_footer">
+                <!-- menu footer -->
+                <div class="row">
+                    <div class="col_1">
+                    <h3>DC COMICS</h3>
+                    <ul>
+                        <li>
+                            <a href="">CHARACTERS</a>
+                        </li>
+                        <li>
+                            <a href="">COMICS</a>
+                        </li>
+                        <li>
+                            <a href="">MOVIES</a>
+                        </li>
+                        <li>
+                            <a href="">TV</a>
+                        </li>
+                        <li>
+                            <a href="">GAMES</a>
+                        </li>
+                        <li>
+                            <a href="">VIDEOS</a>
+                        </li>
+                        <li>
+                            <a href="">NEWS</a>
+                        </li>
+                    </ul>
+                    <h3>SHOP</h3>
+                    <ul>
+                        <li>
+                            <a href="">shop DC</a>
+                        </li>
+                        <li>
+                            <a href="">shop DC Collectibles</a>
+                        </li>
+
+                    </ul>
+
+                </div>
+                <div class="col_2">
+                    <h3>DC </h3>
+                    <ul>
+                        <li>
+                            <a href="">Term of Use</a>
+                        </li>
+                        <li>
+                            <a href="">Privacy policy</a>
+                        </li>
+                        <li>
+                            <a href="">Ad Choise</a>
+                        </li>
+                        <li>
+                            <a href="">Advertising</a>
+                        </li>
+                        <li>
+                            <a href="">Jobs</a>
+                        </li>
+                        <li>
+                            <a href="">Subscriptions</a>
+                        </li>
+                        <li>
+                            <a href="">Talent Workshop</a>
+                        </li>
+                         <li>
+                            <a href="">CPSPC certificates</a>
+                        </li>
+                         <li>
+                            <a href="">Ratings</a>
+                        </li>
+                        <li>
+                            <a href="">Shop Helps</a>
+                        </li>
+                         <li>
+                            <a href="">Contacs Us</a>
+                        </li>
+                    </ul>
+                </div>
+                <div class="col_3">
+                    <h3>SITES</h3>
+                    <ul>
+                        <li>
+                            <a href="">DC</a>
+                        </li>
+                        <li>
+                            <a href="">MAD magazine</a>
+                        </li>
+                        <li>
+                            <a href="">DC kids</a>
+                        </li>
+                         <li>
+                            <a href="">DC Universe</a>
+                        </li>
+                        <li>
+                            <a href="">DC Power Visa</a>
+                        </li>
+                    </ul>
+
+                </div>
+                </div>
+
+            </div>
+            <div id="image_background">
+                <img src="/images/dc-logo-bg.png" alt="">
+            </div>
+        </div>
+    </section>
+    <section id="bottom_footer">
+        <div class="container">
+            <a href="">SIGN-UP NOW</a>
+            <ul>
+                <li>
+                    <span>follow us</span>
+                </li>
+                <li>
+                    <a href="">
+
+                    </a>
+                </li>
+                <li>
+                    <a href="">
+
+                    </a>
+                </li>
+                <li>
+                    <a href="">
+
+                    </a>
+                </li>
+                <li>
+                    <a href="">
+
+                    </a>
+                </li>
+                <li>
+                    <a href="">
+
+                    </a>
+                </li>
+            </ul>
+
+        </div>
+
+    </section>
+
+</footer>
