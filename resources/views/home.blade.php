2fa56d44fa76264686706f2036a56d3169d79b4f
mirkosimonetti <mirko.simonetti.ta@gmail.com>
2022-07-12 16:23:38 +0200

    inserimento card!!!

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -6,22 +6,24 @@
 
 <main>
     <section id="hero">
-        <!-- immgaine hero -->
+        <!-- immagine hero in background-->
     </section>
     <section id="series">
         <div id="current">CURRENT SERIES</div>
     </section>
     <section id="card_container">
         <div class="container">
-            {{-- <div class="card">
-                <div class="box_image">
-                    <img :src="img" :alt="title">
-                </div>
-                <div class="box_title">
-                    <h4>{{title}}</h4>
+            @foreach ($arrComics as $item)
+                <div class="card">
+                    <div class="box_image">
+                         <img src="{{$item['thumb']}}" alt="">
+                    </div>
+                    <div class="box_title">
+                        <h4>{{ $item['title'] }}</h4>
+                    </div>
                 </div>
 
-            </div> --}}
+            @endforeach
         </div>
     </section>
     <section id="row_more">
--- routes/web.php
+++ routes/web.php
@@ -14,6 +14,7 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    $arrComics = config('comics');
-    return view('home');
+    return view('home',[
+        'arrComics'  => config('comics'),
+    ]);
 })->name('home');
